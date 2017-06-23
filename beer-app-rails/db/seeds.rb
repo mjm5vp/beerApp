5573900f35cb66e6adeed7f0ae26ad80d3b797b4
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-06-23 15:54:49 -0400

    API, rails, seed data parsing

---
+++ beer-app-rails/db/seeds.rb
@@ -0,0 +1,63 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'httparty'
+
+Location.destroy_all
+Beer.destroy_all
+Comment.destroy_all
+
+all_locations = []
+all_beers = []
+
+#  194
+# 1.times do |page|
+#   locationsUrl = "http://api.brewerydb.com/v2/locations/?key=9cbedc703b22c38506ed1375ea350381&p=#{page}"
+#   locationsResponse = HTTParty.get(locationsUrl).parsed_response["data"]
+#
+#
+#
+#
+#   for location in locationsResponse do
+#     all_locations.push(location)
+#   end
+# end
+#
+# puts "HTTParty Location Done"
+#
+# for location in all_locations do
+#   this_location = Location.create!(name: location["name"], streetAddress: location["streetAddress"])
+# end
+#
+# puts "Location objects created"
+
+# 1290
+1.times do |page|
+  beersUrl = "http://api.brewerydb.com/v2/beers/?key=9cbedc703b22c38506ed1375ea350381&p=#{page}"
+  beersResponse = HTTParty.get(beersUrl).parsed_response["data"]
+
+  for beer in beersResponse do
+    all_beers.push(beer)
+  end
+end
+
+puts "HTTParty Beer Done"
+
+
+for beer in all_beers do
+  this_beer = Beer.create!(name: beer["name"], description: beer["description"])
+
+end
+
+puts "Beer objects created"
+
+
+
+
+puts all_locations.length
+puts all_beers.length
