39b93bd8e1aa4260b7ebd5108353e4cab9040525
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-06-28 09:29:00 -0400

    richmond only

--- beer-app-rails/db/seeds.rb
+++ beer-app-rails/db/seeds.rb
@@ -22,163 +22,164 @@ all_beers = []
   breweriesResponse = HTTParty.get(breweriesUrl).parsed_response["data"]
 
   for brewery in breweriesResponse do
-    all_breweries.push(brewery)
+    if brewery["locality"] == "Richmond"
+      all_breweries.push(brewery)
 
-    if brewery["brewery"]["images"]
-      icon = brewery["brewery"]["images"]["icon"]
-      medium = brewery["brewery"]["images"]["medium"]
-      large = brewery["brewery"]["images"]["large"]
-      squareMedium = brewery["brewery"]["images"]["squareMedium"]
-      squareLarge = brewery["brewery"]["images"]["squareLarge"]
-    else
-      icon = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
-      medium = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
-      large = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
-      squareMedium = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
-      squareLarge = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
-    end
-
-    this_brewery = Brewery.create!(loc_id: brewery["id"],
-    name: brewery["brewery"]["name"],
-    streetAddress: brewery["streetAddress"],
-    locality: brewery["locality"],
-    country: brewery["country"]["name"],
-    region: brewery["region"],
-    locationTypeDisplay: brewery["locationTypeDisplay"],
-    isClosed: brewery["isClosed"],
-    yearOpened: brewery["yearOpened"],
-    phone: brewery["phone"],
-    website: brewery["website"],
-    hoursOfOperation: brewery["hoursOfOperation"],
-    latitude: brewery["latitude"],
-    longitude: brewery["longitude"],
-    description: brewery["brewery"]["description"],
-    isOrganic: brewery["brewery"]["isOrganic"],
-    icon: icon,
-    medium: medium,
-    large: large,
-    squareMedium: squareMedium,
-    squareLarge: squareLarge,
-    isMassOwned: brewery["brewery"]["isMassOwned"],
-    brandClassification: brewery["brewery"]["brandClassification"]
-
-    # squareLarge: brewery["brewery"]["squareLarge"],
-    # squareLarge: brewery["brewery"]["squareLarge"],
-    # squareLarge: brewery["brewery"]["squareLarge"],
-
-
-    )
-
-    breweryId = brewery["breweryId"]
-    # puts "breweryId: #{breweryId}"
-    breweryBeersUrl = "http://api.brewerydb.com/v2/brewery/#{breweryId}/beers/?key=9cbedc703b22c38506ed1375ea350381"
-    breweriesBeersResponse = HTTParty.get(breweryBeersUrl).parsed_response["data"]
-
-
-
-    if breweriesBeersResponse
-      for beer in breweriesBeersResponse do
-        all_beers.push(beer)
-
-        if beer["style"]
-          category = beer["style"]["category"]["name"]
-          style_name = beer["style"]["shortName"]
-        else
-          category = nil
-          style_name = nil
-        end
-
-        if beer["glass"]
-          glass = beer["glass"]["name"]
-        else
-          glass = nil
-        end
-
-        if beer["available"]
-          available = beer["available"]["name"]
-        else
-          available = nil
-        end
-
-        if beer["srm"]
-          srm_name = beer["srm"]["name"]
-          srm_hex = beer["srm"]["hex"]
-        else
-          srm_name = nil
-          srm_hex = nil
-        end
-
-        case beer["abv"].to_i
-        when 12..50
-          abv_strength = "very high"
-        when 9..11.99
-          abv_strength = "high"
-        when 7..8.99
-          abv_strength = "medium"
-        when 4..6.99
-          abv_strength = "low"
-        when 0..3.99
-          abv_strength = "very low"
-        end
-
-        case beer["ibu"].to_i
-        when 60..500
-          bitterness = "very high"
-        when 45..59.99
-          bitterness = "high"
-        when 30..44.99
-          bitterness = "medium"
-        when 10..29.99
-          bitterness = "low"
-        when 0..9.99
-          bitterness = "very low"
-        end
-
-        # puts "Beer: #{beer["name"]}"
-        this_beer = Beer.create!(
-        name: beer["name"],
-        description: beer["description"],
-        abv: beer["abv"],
-        ibu: beer["ibu"],
-        glasswareId: beer["glasswareId"],
-        glass: glass,
-        srm_name: srm_name,
-        srm_hex: srm_hex,
-        available: available,
-        styleId: beer["styleId"],
-        isOrganic: beer["isOrganic"],
-        hasLabels: beer["hasLabels"],
-        year: beer["year"],
-        status: beer["status"],
-        foodPairings: beer["foodPairings"],
-        servingTemperature: beer["servingTemperature"],
-        servingTemperatureDisplay: beer["servingTemperatureDisplay"],
-        beerVariationId: beer["beerVariationId"],
-        beerVariation: beer["beerVariation"],
-        category: category,
-        style_name: style_name,
-
-        abv_strength: abv_strength,
-        bitterness: bitterness,
-
-        brewery_name: brewery["brewery"]["name"],
-        icon: icon,
-        medium: medium,
-        large: large,
-        squareMedium: squareMedium,
-        squareLarge: squareLarge,
-        brewery_website: brewery["website"],
-
-        brewery: this_brewery
-        )
-
-
-
+      if brewery["brewery"]["images"]
+        icon = brewery["brewery"]["images"]["icon"]
+        medium = brewery["brewery"]["images"]["medium"]
+        large = brewery["brewery"]["images"]["large"]
+        squareMedium = brewery["brewery"]["images"]["squareMedium"]
+        squareLarge = brewery["brewery"]["images"]["squareLarge"]
+      else
+        icon = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
+        medium = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
+        large = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
+        squareMedium = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
+        squareLarge = "http://silhouettesfree.com/holiday-and-festive/saint-patrick-s-day/beer-silhouette-image-2.png"
       end
-    else
-      # puts "This brewery has no beers"
-    end
 
+      this_brewery = Brewery.create!(loc_id: brewery["id"],
+      name: brewery["brewery"]["name"],
+      streetAddress: brewery["streetAddress"],
+      locality: brewery["locality"],
+      country: brewery["country"]["name"],
+      region: brewery["region"],
+      locationTypeDisplay: brewery["locationTypeDisplay"],
+      isClosed: brewery["isClosed"],
+      yearOpened: brewery["yearOpened"],
+      phone: brewery["phone"],
+      website: brewery["website"],
+      hoursOfOperation: brewery["hoursOfOperation"],
+      latitude: brewery["latitude"],
+      longitude: brewery["longitude"],
+      description: brewery["brewery"]["description"],
+      isOrganic: brewery["brewery"]["isOrganic"],
+      icon: icon,
+      medium: medium,
+      large: large,
+      squareMedium: squareMedium,
+      squareLarge: squareLarge,
+      isMassOwned: brewery["brewery"]["isMassOwned"],
+      brandClassification: brewery["brewery"]["brandClassification"]
+
+      # squareLarge: brewery["brewery"]["squareLarge"],
+      # squareLarge: brewery["brewery"]["squareLarge"],
+      # squareLarge: brewery["brewery"]["squareLarge"],
+
+
+      )
+
+      breweryId = brewery["breweryId"]
+      # puts "breweryId: #{breweryId}"
+      breweryBeersUrl = "http://api.brewerydb.com/v2/brewery/#{breweryId}/beers/?key=9cbedc703b22c38506ed1375ea350381"
+      breweriesBeersResponse = HTTParty.get(breweryBeersUrl).parsed_response["data"]
+
+
+
+      if breweriesBeersResponse
+        for beer in breweriesBeersResponse do
+          all_beers.push(beer)
+
+          if beer["style"]
+            category = beer["style"]["category"]["name"]
+            style_name = beer["style"]["shortName"]
+          else
+            category = nil
+            style_name = nil
+          end
+
+          if beer["glass"]
+            glass = beer["glass"]["name"]
+          else
+            glass = nil
+          end
+
+          if beer["available"]
+            available = beer["available"]["name"]
+          else
+            available = nil
+          end
+
+          if beer["srm"]
+            srm_name = beer["srm"]["name"]
+            srm_hex = beer["srm"]["hex"]
+          else
+            srm_name = nil
+            srm_hex = nil
+          end
+
+          case beer["abv"].to_i
+          when 12..50
+            abv_strength = "very high"
+          when 9..11.99
+            abv_strength = "high"
+          when 7..8.99
+            abv_strength = "medium"
+          when 4..6.99
+            abv_strength = "low"
+          when 0..3.99
+            abv_strength = "very low"
+          end
+
+          case beer["ibu"].to_i
+          when 60..500
+            bitterness = "very high"
+          when 45..59.99
+            bitterness = "high"
+          when 30..44.99
+            bitterness = "medium"
+          when 10..29.99
+            bitterness = "low"
+          when 0..9.99
+            bitterness = "very low"
+          end
+
+          # puts "Beer: #{beer["name"]}"
+          this_beer = Beer.create!(
+          name: beer["name"],
+          description: beer["description"],
+          abv: beer["abv"],
+          ibu: beer["ibu"],
+          glasswareId: beer["glasswareId"],
+          glass: glass,
+          srm_name: srm_name,
+          srm_hex: srm_hex,
+          available: available,
+          styleId: beer["styleId"],
+          isOrganic: beer["isOrganic"],
+          hasLabels: beer["hasLabels"],
+          year: beer["year"],
+          status: beer["status"],
+          foodPairings: beer["foodPairings"],
+          servingTemperature: beer["servingTemperature"],
+          servingTemperatureDisplay: beer["servingTemperatureDisplay"],
+          beerVariationId: beer["beerVariationId"],
+          beerVariation: beer["beerVariation"],
+          category: category,
+          style_name: style_name,
+
+          abv_strength: abv_strength,
+          bitterness: bitterness,
+
+          brewery_name: brewery["brewery"]["name"],
+          icon: icon,
+          medium: medium,
+          large: large,
+          squareMedium: squareMedium,
+          squareLarge: squareLarge,
+          brewery_website: brewery["website"],
+
+          brewery: this_brewery
+          )
+
+
+
+        end
+      else
+        # puts "This brewery has no beers"
+      end
+    end
   end
 
 end
